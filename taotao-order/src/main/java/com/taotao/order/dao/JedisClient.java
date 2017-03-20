b912077d7b02234b55919d9d7579ad4cfdd201bd
lingang <lingang@lingang-PC>
2017-03-20 11:48:32 +0800

    创建订单系统工程目录。在该系统下开发： 1、下单功能 2、查看订单列表功能 3、根据订单号查看订单详情功能。

---
+++ taotao-order/src/main/java/com/taotao/order/dao/JedisClient.java
@@ -0,0 +1,13 @@
+package com.taotao.order.dao;
+
+public interface JedisClient {
+	String set(String key,String value);
+    String get(String key);
+    long hset(String hkey,String key,String value);//哈希值
+    String hget(String hkey,String key);
+    long incr(String key);
+    long expire(String key, int second);//设置key的有效时间
+    long ttl(String key);//查询key是否过期
+    long del(String key);// 删除key
+    long hdel(String hkey,String key);
+} 
